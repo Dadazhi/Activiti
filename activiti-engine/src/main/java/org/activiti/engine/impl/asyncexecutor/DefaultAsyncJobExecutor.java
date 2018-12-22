6e18f857b627a41d4ee0e178e3dfef0115940343
Igor Dianov <igor@dianov.com>
2018-12-21 21:46:14 -0800

    Revert "Try increase default corePoolSize to 4;"
    This reverts commit 676afd7c07568293eb129e877dd6556a45bbb8ff.

--- activiti-engine/src/main/java/org/activiti/engine/impl/asyncexecutor/DefaultAsyncJobExecutor.java
+++ activiti-engine/src/main/java/org/activiti/engine/impl/asyncexecutor/DefaultAsyncJobExecutor.java
@@ -42,8 +42,7 @@ public class DefaultAsyncJobExecutor implements AsyncExecutor {
   /**
    * The minimal number of threads that are kept alive in the threadpool for job execution
    */
-  //protected int corePoolSize = Runtime.getRuntime().availableProcessors() == 1 ? 1 : 2;
-  protected int corePoolSize = 4;
+  protected int corePoolSize = Runtime.getRuntime().availableProcessors() == 1 ? 1 : 2;
 
   /**
    * The maximum number of threads that are kept alive in the threadpool for job execution
